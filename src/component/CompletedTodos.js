512652a86c98e077bfa9fe4cfd662c2aeaee5a92
azufb <curiousazunas@gmail.com>
2021-05-06 21:42:33 +0900

    全ての完了タスク削除のボタンの非活性化対応

--- src/component/CompletedTodos.js
+++ src/component/CompletedTodos.js
@@ -6,6 +6,7 @@ import { DELETE_COMPLETED_TODOS } from "../actions";
 const CompletedTodos = () => {
     const { state, dispatch } = useContext(AppContext);
 
+
     const deleteCompletedTodos = () => {
         const confirmed = window.confirm("完了したタスクたちを全て削除しても良いですか？");
 
@@ -16,10 +17,13 @@ const CompletedTodos = () => {
         }
     }
 
+    const disableDeleteCompleted = state.completedTodos.length === 0;
+
+
     return (
         <div>
             <h3>完了！</h3>
-            <button type="button" className="btn btn-danger mb-3" onClick={deleteCompletedTodos}>完了したタスクたちを削除</button>
+            <button type="button" className="btn btn-danger mb-3" onClick={deleteCompletedTodos} disabled={disableDeleteCompleted}>完了したタスクたちを削除</button>
             <div className="list-group">
                 {
                     state.completedTodos.map((completedTodo, index) => <CompletedTodo key={index} completedTodo={completedTodo} />)
